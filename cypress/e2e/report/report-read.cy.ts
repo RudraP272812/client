fac4187f64cb7acd4dd5c152499ac5533945143e
RudraP272812 <rudrapatel201@gmail.com>
2025-11-26 10:33:01 -0500

    Lab 14

--- cypress/e2e/report/report-read.cy.ts
+++ cypress/e2e/report/report-read.cy.ts
@@ -6,6 +6,6 @@ describe('Report - View Report', () => {
     cy.wait(1000);
     cy.get('mat-select[formControlName=employee]').click().get('mat-option').contains('Patel').click();
     cy.get('mat-select[formControlName=report]').click().get('mat-option').last().click();
-    cy.contains('EXPENSE OF YOUR EMPLOYEE');
+    cy.contains('Park');
   });
 });
